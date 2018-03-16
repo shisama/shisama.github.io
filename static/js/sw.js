4ab4aab05eca7e87b5d6583e68d0bfb31b249481
Masashi Hirano <cherrydog07@gmail.com>
2018-03-16 23:30:43 +0900

    Add: serviceworker

---
+++ static/js/sw.js
@@ -0,0 +1,16 @@
+var CACHE_NAME = 'shisama.github.io-v1';
+var urlsToCache = [
+  '/',
+  '/static/js/sw.js'
+];
+
+self.addEventListener('install', function(event) {
+  // インストール手順を実行する
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
