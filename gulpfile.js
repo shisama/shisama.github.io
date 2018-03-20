e52b2714441b959c438a0cf6e226a5d9325bd3d2
Masashi Hirano <cherrydog07@gmail.com>
2018-03-21 01:36:28 +0900

    Improve: use pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 const gulp = require('gulp');
 const inline_source = require('gulp-inline-source');
+const pug = require('gulp-pug');
+const path = require('path');
 
 gulp.task("html", function() {
-  gulp.src("src/index.html")
-  // CSS/JSを埋め込む
-  .pipe(inline_source())
-  .pipe(gulp.dest("./"));
+  const src = path.resolve('src');
+  const dest = path.resolve('./');
+
+  gulp.src(path.join(src, '*.pug'))
+      .pipe(pug({pretty: false}))
+      .pipe(gulp.dest(dest))
+      .pipe(inline_source())
+      .pipe(gulp.dest(dest));
 });
