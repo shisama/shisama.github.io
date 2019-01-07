67e22b8bae709083d5c78f3f672cbba1290a3eb2
Masashi Hirano <cherrydog07@gmail.com>
2019-01-08 01:59:53 +0900

    Upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,26 +9,9 @@ gulp.task("html", function() {
   const src = path.resolve('src');
   const dest = path.resolve('./');
 
-  gulp.src(path.join(src, '*.pug'))
+  return gulp.src(path.join(src, '*.pug'))
       .pipe(pug({pretty: false}))
       .pipe(gulp.dest(dest))
       .pipe(inline_source())
       .pipe(gulp.dest(dest));
-});
-gulp.task('retire', function() {
-  // Spawn Retire.js as a child process
-  // You can optionally add option parameters to the second argument (array)
-  var child = spawn('retire', ['-p'], {cwd: process.cwd()});
-
-  child.stdout.setEncoding('utf8');
-  child.stdout.on('data', function (data) {
-    gutil.log(data);
-  });
-
-  child.stderr.setEncoding('utf8');
-  child.stderr.on('data', function (data) {
-    gutil.log(gutil.colors.red(data));
-    gutil.beep();
-    process.exit(1);
-  });
 });
