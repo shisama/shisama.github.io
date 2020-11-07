25d6b2d588668a376b8440eda1df008a3ff9f9b2
shisama <shisama07@gmail.com>
2020-11-08 02:19:43 +0900

    chore: format with prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
-const gulp = require('gulp');
-const inline_source = require('gulp-inline-source');
-const pug = require('gulp-pug');
-const path = require('path');
+const gulp = require("gulp");
+const inline_source = require("gulp-inline-source");
+const pug = require("gulp-pug");
+const path = require("path");
 
-gulp.task("html", function() {
-  const src = path.resolve('src');
-  const dest = path.resolve('./docs');
+gulp.task("html", function () {
+  const src = path.resolve("src");
+  const dest = path.resolve("./docs");
 
-  return gulp.src(path.join(src, "pug", '*.pug'))
-      .pipe(pug({pretty: false}))
-      .pipe(inline_source())
-      .pipe(gulp.dest(dest));
+  return gulp
+    .src(path.join(src, "pug", "*.pug"))
+    .pipe(pug({ pretty: false }))
+    .pipe(inline_source())
+    .pipe(gulp.dest(dest));
 });
--- workbox-config.js
+++ workbox-config.js
@@ -1,26 +1,24 @@
 const path = require("path");
 
 module.exports = {
-  "globDirectory": "./docs",
-  "globPatterns": [
-    "**/*.{ico,png,svg,html,css,js}",
-    "*.webmanifest"
-  ],
-  "globIgnores": [
+  globDirectory: "./docs",
+  globPatterns: ["**/*.{ico,png,svg,html,css,js}", "*.webmanifest"],
+  globIgnores: [
     "gulpfile.js",
     "workbox-config.js",
     "src/**/**.*",
-    "**/node_modules/**/**.*"
+    "**/node_modules/**/**.*",
   ],
-  "swDest": path.resolve(__dirname, "docs", "sw.js"),
-  "runtimeCaching": [{
-    urlPattern: new RegExp('^https://cdn.ampproject.org'),
-    handler: "CacheFirst",
-    options: {
-      cacheableResponse: {
-        statuses: [0, 200]
-      }
-    }
-
-  }]
+  swDest: path.resolve(__dirname, "docs", "sw.js"),
+  runtimeCaching: [
+    {
+      urlPattern: new RegExp("^https://cdn.ampproject.org"),
+      handler: "CacheFirst",
+      options: {
+        cacheableResponse: {
+          statuses: [0, 200],
+        },
+      },
+    },
+  ],
 };
